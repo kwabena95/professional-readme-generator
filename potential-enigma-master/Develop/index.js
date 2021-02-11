249fb512c21782606c90f4d2ffae26068efc19e6
ebenezer <e.kankam95@hotmail.com>
2021-02-11 10:50:41 -0600

    initial setup

---
+++ potential-enigma-master/Develop/index.js
@@ -0,0 +1,160 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkDown = require('./utils/generateMarkdown');
+// const { resolve } = require('path');
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of project');
+                    return false;
+                }
+
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter description of your project. (Required)',
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                } else {
+                    console.log('Project description is required!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTableContent',
+            message: 'Do you want to add table of contents (y/n)',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'tableContent',
+            message: 'Provide table of contents.',
+            when: ({ confirmTableContent }) => confirmTableContent
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide step-by-step description of how to get the development environment running. (Required)',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Installation process is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use. Include screenshots as needed. Add a filepath of screenshot (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide filepath.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCredits',
+            message: 'Were there collaborators on this projects? (y/n)',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List collaborators.',
+            when: ({ confirmCredits }) => confirmCredits
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose license(s). (Required)',
+            choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License'],
+            validate: lincenseInput => {
+                if (lincenseInput) {
+                    return true;
+                } else {
+                    console.log('Please select lincense(s)');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project? (y/n)',
+            default: true
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContribution',
+            message: 'Would you like other developers to contribute to your application?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Provide guidelines to application:',
+            when: ({ confrimContribution }) => confrimContribution
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them. (Required)',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please provide tests for your application');
+                    return false;
+                }
+            }
+        }
+
+    ])
+};
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created'
+            });
+        })
+    }).catch(err => console.log(err));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    questions()
+        // .then(readme => generateMarkDown(readme))
+        .then(writeToFile('./README', generateMarkDown(data)));
+}
+
+// Function call to initialize app
+init();
---
+++ potential-enigma-master/Develop/utils/generateMarkdown.js
@@ -0,0 +1,73 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) return '';
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) return '';
+  return `
+    [https://choosealicense.com/]
+  `;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) return '';
+
+  return `
+    ## License
+    Use link this link if you need help choosing a license ${renderLicenseLink()}
+    ${data.license}
+  `
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+    ${data.map(({ title, description, installation, usage, credits, feature, contribution, tests }) => {
+    return `
+        # ${title}
+
+        ## Description
+        ${description}
+    
+        ## Table of Contents (Optional)
+        If your README is very long, add a table of contents to make it easy for users to find what they need.
+          * [${tableContent}] (# ${tableContent})
+          * [${usage}] (# ${usage})
+          * [${credits}] (# ${credits})
+          * [${license}] (# ${license})
+    
+        ## Installation
+        ${installation}
+    
+        ## Usage
+        ${usage}
+    
+        ## Credits
+        ${credits}
+    
+        ${renderLicenseSection()}
+    
+        ## Features
+        ${feature}
+    
+        ## Contributions
+        ${contribution}
+    
+        ## Tests
+        ${tests}
+    
+    
+      `;
+
+  })}
+   `
+}
+
+module.exports = generateMarkdown;
